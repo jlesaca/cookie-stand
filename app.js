56e1ed4f85117bec01d997e1f78659936f256fc6
Jordan <JordanLesaca@Gmail.Com>
2017-11-13 13:48:18 -0800

    created objects for each store

---
+++ app.js
@@ -0,0 +1,29 @@
+var firstAndPike = {
+  min: 23,
+  max: 65,
+  avgCookieSale: 6.3
+};
+
+var seaTacAirport = {
+  min: 3,
+  max: 24,
+  avgCookieSale: 1.2
+};
+
+var seattleCenter = {
+  min: 11,
+  max: 38,
+  avgCookieSale: 3.7
+};
+
+var capitolHill = {
+  min: 20,
+  max: 38,
+  avgCookieSale: 2.3
+};
+
+var alki = {
+  min: 2,
+  max: 16,
+  avgCookieSale: 4.6
+};
