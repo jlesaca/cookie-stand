a1dea2f992cc042ae6649115d1f795a782f8c495
Jordan <JordanLesaca@Gmail.Com>
2017-11-13 22:28:00 -0800

    changed html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var firstAndPike = {
   min: 23,
   max: 65,
   avgCookieSale: 6.3,
-  avgCust: function() {
+  avgCust: function () {
     return Math.random() * (this.max - this.min) + this.min;
   },
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
