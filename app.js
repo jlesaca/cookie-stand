154b4afc6d03daaf0055ae96878148bcc9edc3d0
Jordan <JordanLesaca@Gmail.Com>
2017-11-15 12:57:45 -0800

    added function for get hourly cookies

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var stores = [];
 
 function Store(name, min, max, avgCookie) { //constructor
   this.name = name;
-  this.min = min;
-  this.max = max;
-  this.avgCookie = avgCookie;
+  this.minCust = min;
+  this.maxCust = max;
+  this.avgCookies = avgCookie;
+  this.hourlyCookies = [];
+  this.dailyTotal = 0;
   stores.push(this);
+  this.getHourlyCookies();
 }
 
 new Store('1st and Pike', 23, 65, 6.3);
@@ -16,6 +19,13 @@ new Store('seaTacAirport', 3, 24, 1.2);
 new Store('seattleCenter', 11, 38, 3.7);
 new Store('capitolHill', 20, 38, 2.3);
 new Store('alki', 2, 16, 4.6);
+
+Store.prototype.getHourlyCookies = function() {
+  for(var i = 0; i < hours.length; i++){
+    this.hourlyCookies.push(Math.floor(this.getCustomersPerHour() * this.avgCookies));
+    this.dailyTotal += this.hourlyCookies[i];
+  }
+};
 
 Store.prototype.ranNumCust = function() { //for avg customers
   return Math.round(Math.random() * (this.max - this.min) + this.min);
@@ -64,73 +74,3 @@ Store.prototype.cPerHour = function() {
   tdEl.textContent = totalDay;
   tableEL.appendChild(trTotal);
 };
-
-//
-// var seaTacAirport = {
-//   min: 3,
-//   max: 24,
-//   avgCookieSale: 1.2,
-//   avgCust: function () {
-//     return Math.random() * (this.max - this.min) + this.min;
-//   },
-//   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-//   cookiesPerHour: function(){
-//     var output = [];
-//     for(var i = 0; i <= this.hours.length; i++) {
-//       output.push(Math.round(this.avgCookieSale * this.avgCust()));
-//     }
-//     return output;
-//   },
-// };
-//
-// var seattleCenter = {
-//   min: 11,
-//   max: 38,
-//   avgCookieSale: 3.7,
-//   avgCust: function () {
-//     return Math.random() * (this.max - this.min) + this.min;
-//   },
-//   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-//   cookiesPerHour: function(){
-//     var output = [];
-//     for(var i = 0; i <= this.hours.length; i++) {
-//       output.push(Math.round(this.avgCookieSale * this.avgCust()));
-//     }
-//     return output;
-//   },
-//
-// };
-//
-// var capitolHill = {
-//   min: 20,
-//   max: 38,
-//   avgCookieSale: 2.3,
-//   avgCust: function () {
-//     return Math.random() * (this.max - this.min) + this.min;
-//   },
-//   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-//   cookiesPerHour: function(){
-//     var output = [];
-//     for(var i = 0; i <= this.hours.length; i++) {
-//       output.push(Math.round(this.avgCookieSale * this.avgCust()));
-//     }
-//     return output;
-//   },
-// };
-//
-// var alki = {
-//   min: 2,
-//   max: 16,
-//   avgCookieSale: 4.6,
-//   avgCust: function () {
-//     return Math.random() * (this.max - this.min) + this.min;
-//   },
-//   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'],
-//   cookiesPerHour: function(){
-//     var output = [];
-//     for(var i = 0; i <= this.hours.length; i++) {
-//       output.push(Math.round(this.avgCookieSale * this.avgCust()));
-//     }
-//     return output;
-//   },
-// };
